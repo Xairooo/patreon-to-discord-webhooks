7ee5713853a3bbe60a8ef0102b263a7c84b4b0df
Xairooo <Xairooo@users.noreply.github.com>
2022-08-15 16:51:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $secret_webhook_id = "secret";
 $discord_webhook = "URL";
 
 // post to discord snippet from https://www.reddit.com/r/discordapp/comments/58hry5/simple_php_function_for_posting_to_webhook/
+
 function postToDiscord($message, $discord_webhook) {
     $data = array("content" => $message, "username" => "Patreon Bot");
     $curl = curl_init($discord_webhook);
@@ -13,7 +14,7 @@ function postToDiscord($message, $discord_webhook) {
     curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
     curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    
+
     $res = curl_exec($curl);
     return $res;
 }
@@ -36,7 +37,7 @@ if(!function_exists('hash_equals')) {
 $data = @file_get_contents('php://input');
 // decode json post data to arrays
 $event_data = json_decode($data, true);
-
+file_put_contents('./log_'.date("j.n.Y").'.log', $data, FILE_APPEND);
 // also get the headers patreon sends
 $X_Patreon_Event     = $_SERVER['HTTP_X_PATREON_EVENT'];
 $X_Patreon_Signature = $_SERVER['HTTP_X_PATREON_SIGNATURE'];
@@ -48,9 +49,11 @@ if (!hash_equals($X_Patreon_Signature, $signature)) {
 }
 
 // get all the user info
-$pledge_amount = $event_data['data']['attributes']['amount_cents'];
+$pledge_amount = number_format(($event_data['data']['attributes']['amount_cents'] /100), 2, '.', ' ');
+$currency = $event_data['data']['attributes']['currency'];
 $patron_id     = $event_data['data']['relationships']['patron']['data']['id'];
 $campaign_id   = $event_data['data']['relationships']['campaign']['data']['id'];
+$reward_id   = $event_data['data']['relationships']['reward']['data']['id'];
 
 foreach ($event_data['included'] as $included_data) {
     if ($included_data['type'] == 'user' && $included_data['id'] == $patron_id) {
@@ -58,6 +61,9 @@ foreach ($event_data['included'] as $included_data) {
     }
     if ($included_data['type'] == 'campaign' && $included_data['id'] == $campaign_id) {
         $campaign_data = $included_data;
+    }
+    if ($included_data['type'] == 'reward' && $included_data['id'] == $reward_id) {
+        $reward_data = $included_data;
     }
 }
 
@@ -67,14 +73,19 @@ $patron_fullname = $user_data['attributes']['full_name'];
 $campaign_sum    = $campaign_data['attributes']['pledge_sum'];
 $patron_count    = $campaign_data['attributes']['patron_count'];
 
+$itemTitle = $reward_data['attributes']['title'];
+
 // send event to discord
 if ($X_Patreon_Event == "pledges:create") {
-    postToDiscord(":star: " . $patron_fullname . " just pledged for $" . number_format(($pledge_amount /100), 2, '.', ' ') . "! <" . $patron_url . "> - New total: $" . number_format(($campaign_sum / 100), 2, '.', ' ') . " by " . $patron_count . " patreons", $discord_webhook);
+    $text = ":euro: $patron_fullname just bought $itemTitle for $currency $pledge_amount";
 } else if ($X_Patreon_Event == "pledges:delete") {
-    postToDiscord(":disappointed: " . $patron_fullname . " just removed their pledge! <" . $patron_url . "> - New total: $" . number_format(($campaign_sum / 100), 2, '.', ' ') . " by " . $patron_count . " patreons", $discord_webhook);
+    $text = ":disappointed: $patron_fullname just removed their pledge!";
 } else if ($X_Patreon_Event == "pledges:update") {
-    postToDiscord(":open_mouth: " . $patron_fullname . " just updated their pledge to $" . number_format(($pledge_amount /100), 2, '.', ' ') . "! <" . $patron_url . "> - New total: $" . number_format(($campaign_sum / 100), 2, '.', ' ') . " by " . $patron_count . " patreons", $discord_webhook);
+    $text = ":open_mouth: $patron_fullname just updated their pledge to $currency $pledge_amount";
 } else {
-    postToDiscord($X_Patreon_Event . ": something happened with Patreon ¯\_(ツ)_/¯", $discord_webhook);
+    $text = $X_Patreon_Event . ": something happened with Patreon ¯\_(ツ)_/¯";
 }
+
+postToDiscord($text, $discord_webhook);
+
 
